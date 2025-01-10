16db3e6af26ea48ec2d0c2646f96f61a6e75b0f9
OXIGO121W <166754986+OXIGO121W@users.noreply.github.com>
2025-01-10 11:15:00 +0700

    change

--- Array Leetcode easy/max_sub.cpp
+++ Array Leetcode easy/max_sub.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-    bool isPalindrome(long long x) {
-        if(x < 0) return false;
-        long long buffer = x;
-        long long ans = 0;
-        while(buffer)
-        {
-            ans = ans*10 + buffer %10;
-            buffer/=10;
-        }
-       return ans == x;
+    int maxSubArray(vector<int>& nums) {
+        int max_sum = nums[0];
+        int n = nums.size();
+        int cur_sum = 0;
+            for(auto x : nums)
+            {
+                if(cur_sum < 0)
+                {
+                    cur_sum = 0;
+                }
+                cur_sum += x;
+                if(cur_sum > max_sum) max_sum = cur_sum;
+            }
+       
+
+        return max_sum;
     }
 };
